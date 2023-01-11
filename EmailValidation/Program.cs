db545e5d937b6a58e9b55e0f9cd74033d4655dfe
Shreekrisna <shree.kris810@gmail.com>
2023-01-11 12:48:01 +0545

    Commit added in comment section

--- EmailValidation/Program.cs
+++ EmailValidation/Program.cs
@@ -9,16 +9,19 @@ namespace EmailValidation
         {
             Console.WriteLine("Enter a email");
             string emailInput = Console.ReadLine();
-           // string emailInput = "sks.kris810@gmail.com";
+            // string emailInput = "sks.kris810@gmail.com";
             bool email = IsValidEmail(emailInput);
-           //OR Console.WriteLine(IsValidEmail(emailInput));
-            Console.WriteLine("The typed email is {0}",email);
+            //OR Console.WriteLine(IsValidEmail(emailInput)); commit
+            Console.WriteLine("The typed email is {0}", email);
+
         }
 
         public static bool IsValidEmail(string emailInput)
         {
-            Regex re = new Regex(@"([a-z][a-z0-9.]*[@][a-z]{5,}[.][a-z]{3,})",RegexOptions.IgnoreCase);
+            Regex re = new Regex(@"([a-z][a-z0-9.]*[@][a-z]{5,}[.][a-z]{3,})", RegexOptions.IgnoreCase);
             return re.IsMatch(emailInput);
         }
+
+       
     }
 }
